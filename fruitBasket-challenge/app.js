9332a0bc0b37714b942447f9a9a2a59b8e3b5b9f
sdevishal <vishalmishrasde@gmail.com>
2025-06-19 16:00:42 +0530

    added fruitBasket challenge

---
+++ fruitBasket-challenge/app.js
@@ -0,0 +1,9 @@
+/* You have a basket of apples, but you decide to give some away to your neighbor. How many apples are left in your basket?
+Challenge:
+Write a function that subtracts the number of apples given away from the total apples you started with, then returns the remaining apples. */
+
+function remainingApples(totalApples, givenAway) {
+return totalApples - givenAway
+
+}
+console.log(remainingApples(20, 5)) //15
